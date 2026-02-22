cb396f0f472969cd5ec4ee60fd45e7634c9a9d52
MandaHarou <Mandaharou>
2026-02-22 18:44:56 +0300

    Rename file descriptor variables for consistency
    Apply a more consistent naming convention to file descriptors to improve code readability and clarity.

--- copy.c
+++ copy.c
@@ -10,31 +10,31 @@ int main(int argc, char *argv[]){
     char errorRead[] = "Eroor: error read file, its dosnt exit ";
     char buf[1024];
     size_t nblus;
-    int fileread = open(argv[1], O_RDONLY, 0666);
-    int filecreat = open(argv[2], O_WRONLY | O_EXCL | O_CREAT, 0666);
+    int fileReadD = open(argv[1], O_RDONLY, 0666);
+    int fileCreatD = open(argv[2], O_WRONLY | O_EXCL | O_CREAT, 0666);
 
     if(argc < 2){
         write(1,"Argument", 8);
         return 1;
     }
-    if(fileread < 0){
+    if(fileReadD < 0){
        int size = sizeof(errorRead);
        write(1, errorRead, size);
        return 2;
     }
 
-   while((nblus = read(fileread, buf, 100)) > 0){
+   while((nblus = read(fileReadD, buf, 100)) > 0){
         write(1, buf, nblus);
-        if(filecreat < 0){
+        if(fileCreatD < 0){
             char errorOpen[] = "erro openign file";
             int size = sizeof(errorOpen);
             write(1, errorOpen, size);
             return 3;
         }
-        write(filecreat, buf, nblus);
+        write(fileCreatD, buf, nblus);
 
    }
-    close(fileread);
-    close(filecreat);
+    close(fileReadD);
+    close(fileCreatD);
     return 0;
 }
